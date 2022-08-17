0c52864ef9733f35a468856b8e8135907dd83746
EViasValdez <eduardoviasvaldez@yahoo.com.mx>
2022-08-16 21:08:11 -0500

    Add file via upload

---
+++ 2622/Conversiones/Conversiones.js
@@ -0,0 +1,16 @@
+﻿function accion(){
+    conversion();
+}
+
+function conversion(){
+    let numero = document.getElementById("Numero").value;
+
+    if (numero = ""){
+      alert("No ha ingresado ningun valor");
+    }
+    else
+    {
+      console.log(Number(numero).toString(2));
+      // document.getElementById("Resultado").value=(numero);
+    }
+}
