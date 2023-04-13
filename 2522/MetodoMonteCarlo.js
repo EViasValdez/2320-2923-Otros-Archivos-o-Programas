38a5ea897e1dcaee3da47fb51d2bc442e5a31055
EViasValdez <eduardoviasvaldez@yahoo.com.mx>
2023-04-13 09:46:26 -0600

    Add files via upload

---
+++ 2522/MetodoMonteCarlo.js
@@ -0,0 +1,21 @@
+var LimInferior = 0;
+var LimSuperior = 0.16;
+
+function Funcion1() {
+    var Probabilidad = parseFloat(document.getElementById("ValorLados")).value;
+    var Division = parseFloat(document.getElementById("ValorDivision")).value;
+
+    if (Probabilidad == "" || Division == "") {
+        alert("No ha ingresado ningun valor");
+    }
+    else{
+        var RProb;
+
+        RProb = Probabilidad + Division;
+        alert(RProb);
+        console.log(RProb);
+    }
+}
+function click() {
+    Funcion1();
+}
