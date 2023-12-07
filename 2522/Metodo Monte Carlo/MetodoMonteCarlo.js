5f25d4835be13fe8717d17293cd625c221ef954c
EViasValdez <eduardoviasvaldez@yahoo.com.mx>
2023-12-06 22:02:05 -0600

    Add files via upload

--- 2522/Metodo Monte Carlo/MetodoMonteCarlo.js
+++ 2522/Metodo Monte Carlo/MetodoMonteCarlo.js
@@ -5,10 +5,11 @@ function Funcion1() {
     var Probabilidad = parseFloat(document.getElementById("ValorLados")).value;
     var Division = parseFloat(document.getElementById("ValorDivision")).value;
 
-    if (Probabilidad == "" || Division == "") {
+    if (Probabilidad == "" || Division == "")
+    {
         alert("No ha ingresado ningun valor");
     }
-    else{
+    else {
         var RProb;
 
         RProb = Probabilidad + Division;
