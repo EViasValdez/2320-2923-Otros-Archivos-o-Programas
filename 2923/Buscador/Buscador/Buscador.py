d6e2a40edf4bdd4c7c87552c7f58b7626bac7a64
Eduardo <eduardoviasvaldez@yahoo.com.mx>
2024-01-11 10:46:42 -0600

    Add files via upload

--- 2923/Buscador/Buscador/Buscador.py
+++ 2923/Buscador/Buscador/Buscador.py
@@ -1,33 +1,33 @@
 from unittest import result
 import mysql.connector
-from CLASES.table_info import TableInfo
+from CLASES.Table_Info import TableInfo
 
-# Uso de la clase TableInfo
-table_info = TableInfo('localhost', 'root', '', 'odapas')
-num_fields = table_info.get_num_fields('sys_roles')
+# Uso de la clase TableInfo.
+Table_Info = TableInfo('localhost', 'root', '', 'odapas')
+num_fields = Table_Info.get_num_fields('sys_roles')
 print(f"La tabla sys_roles tiene {num_fields} campos.")
 
-# Obtener los resultados de la consulta
-results = table_info.results()
+# Obtener los resultados de la consulta.
+Results = Table_Info.Results()
 
-# Crear un conjunto vacío para almacenar los resultados
+# Crear un conjunto vacio para almacenar los resultados.
 result_set = set()
 
-# Iterar sobre los resultados y agregarlos al conjunto
-for row in results:
+# Iterar sobre los resultados y agregarlos al conjunto.
+for row in Results:
     result_set.add(row)
 
-# Convertir el conjunto a una lista
+# Convertir el conjunto a una lista.
 result_list = list(result_set)
 
-# Imprimir la lista resultante
+# Imprimir la lista resultante.
 print(result_list)
 
-# Obtener los resultados de la consulta
-results = table_info.results()
+# Obtener los resultados de la consulta.
+Results = Table_Info.Results()
 
-# Obtener la cantidad de resultados
-num_results = len(results)
+# Obtener la cantidad de resultados.
+Num_results = len(Results)
 
-# Imprimir la cantidad de resultados
-print(f"Hay {num_results} resultados en el campo sys_roles.")
+# Imprimir la cantidad de resultados.
+print(f"Hay {Num_results} resultados en el campo sys_roles.")
