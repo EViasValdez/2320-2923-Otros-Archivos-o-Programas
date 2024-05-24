68e45b2495ecf848b029865129404edadf13890e
Eduardo <eduardoviasvaldez@yahoo.com.mx>
2024-05-24 10:26:06 -0600

    Add files via upload

--- 2722/Expresiones/Expresiones/obj/Debug/net6.0/Expresiones.AssemblyInfo.cs
+++ 2722/Expresiones/Expresiones/obj/Debug/net6.0/Expresiones.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Expresiones")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff15086213deba26969d44bccd2b64e357493b49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8897f3a98d03cc3e1cb48bead9e2483918f97a61")]
 [assembly: System.Reflection.AssemblyProductAttribute("Expresiones")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Expresiones")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
