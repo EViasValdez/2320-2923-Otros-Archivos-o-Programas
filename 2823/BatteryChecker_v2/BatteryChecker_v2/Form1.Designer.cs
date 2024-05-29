2d831ff2d278319b0f7038111b2bbb58d7b41c2d
Eduardo <eduardoviasvaldez@yahoo.com.mx>
2024-05-29 10:39:23 -0600

    Add files via upload

---
+++ 2823/BatteryChecker_v2/BatteryChecker_v2/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace BatteryChecker_v2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new Button();
+            progressBar1 = new ProgressBar();
+            label1 = new Label();
+            label2 = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            label3 = new Label();
+            label4 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Transparent;
+            button1.Font = new Font("Showcard Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.ForeColor = Color.Aqua;
+            button1.Location = new Point(215, 163);
+            button1.Margin = new Padding(2, 2, 2, 2);
+            button1.Name = "button1";
+            button1.Size = new Size(114, 20);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(29, 78);
+            progressBar1.Margin = new Padding(2, 2, 2, 2);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(498, 20);
+            progressBar1.TabIndex = 1;
+            progressBar1.Click += progressBar1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Showcard Gothic", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = Color.Aqua;
+            label1.Location = new Point(29, 52);
+            label1.Margin = new Padding(2, 0, 2, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(86, 28);
+            label1.TabIndex = 2;
+            label1.Text = "label1";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Showcard Gothic", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.ForeColor = SystemColors.ControlLightLight;
+            label2.Location = new Point(142, 15);
+            label2.Margin = new Padding(2, 0, 2, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(256, 28);
+            label2.TabIndex = 3;
+            label2.Text = "Inspector de bateria";
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Showcard Gothic", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.ForeColor = Color.Aqua;
+            label3.Location = new Point(419, 46);
+            label3.Margin = new Padding(2, 0, 2, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(88, 28);
+            label3.TabIndex = 4;
+            label3.Text = "label3";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Showcard Gothic", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.ForeColor = Color.Aqua;
+            label4.Location = new Point(240, 109);
+            label4.Margin = new Padding(2, 0, 2, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(89, 28);
+            label4.TabIndex = 5;
+            label4.Text = "label4";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(560, 270);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(progressBar1);
+            Controls.Add(button1);
+            Margin = new Padding(2, 2, 2, 2);
+            Name = "Form1";
+            Text = "Inspector de bateria";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private ProgressBar progressBar1;
+        private Label label1;
+        private Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private Label label3;
+        private Label label4;
+    }
+}
---
+++ 2823/BatteryChecker_v2/BatteryChecker_v2/elements.cs
@@ -0,0 +1,60 @@
+﻿using IAupt.uptRNA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BatteryChecker_v2
+{
+    public class elements
+    {
+        public string texto="";
+        public bool check;
+        public void training()
+        {
+          PerceptronMultiCapa rna = new PerceptronMultiCapa(@"InstructivoPerceptronMulticapa.pml");
+          rna.entrenar();
+        }
+        public void recognize(int b)
+        {
+          PerceptronMultiCapa rna = new PerceptronMultiCapa(@"InstructivoPerceptronMulticapa.ppm");
+
+           double[] x = { b };
+           rna.reconocer(x);
+
+           double[,] y = rna.y;
+
+            switch (y[0, 0])
+            {
+                default:
+                    texto = "no hay datos";
+                    break;
+                case 1:
+                    texto = "Muy baja";
+                    break;
+                case 2:
+                    texto = "Baja";
+                    break;
+                case 3:
+                    texto = "Media";
+                    break;
+                case 4:
+                    texto = "Alta";
+                    break;
+                case 5:
+                    texto = "Muy alta";
+                    break;
+            }
+        }
+        public void checker(bool element)
+        {
+
+        }
+        public bool checking(bool element) 
+        {
+            
+            return this.check;
+        }
+    }
+}
