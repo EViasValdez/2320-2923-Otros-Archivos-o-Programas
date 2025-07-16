507fba1142f417fefdc0f2995efbe70a26828d12
Eduardo <eduardoviasvaldez@yahoo.com.mx>
2025-07-15 19:30:27 -0600

    Add files via upload

---
+++ 2923/Mineria de datos/SpiderDemo/SpiderDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using SpiderDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpiderDemo
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Spider Busqeuda = new Spider();
+            List<Car> Car  = await Busqueda.Start();
+            
+            Console.WriteLine();
+        }
+    }
+}
