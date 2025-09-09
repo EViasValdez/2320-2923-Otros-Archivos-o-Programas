d5148fc1e7f1fc0acb9d54c9dd493e01fc781b19
Eduardo <eduardoviasvaldez@yahoo.com.mx>
2025-09-08 18:45:16 -0600

    Add files via upload

--- 2923/Mineria de datos/PruebaML/PruebaML/obj/Debug/net8.0/PruebaML.AssemblyInfo.cs
+++ 2923/Mineria de datos/PruebaML/PruebaML/obj/Debug/net8.0/PruebaML.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PruebaML")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c245e0713ef81ad9262570e4e1288e252ceeb7b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+090574036e1b8ef3ecb7d078fdbf92262ad19409")]
 [assembly: System.Reflection.AssemblyProductAttribute("PruebaML")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PruebaML")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
