220847cedd2177189221093421bcbd959f94186b
Eduardo <eduardoviasvaldez@yahoo.com.mx>
2026-06-10 21:17:04 -0600

    Add files via upload

--- 2722/WebScraping/WebScraping/obj/Release/net8.0/WebScraping.AssemblyInfo.cs
+++ 2722/WebScraping/WebScraping/obj/Release/net8.0/WebScraping.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebScraping")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20418d706ef741a410cd3a67f6d68c86bbb563ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+003a2f5ca43095b5a4a8cfb6337d8c9cf9cd8492")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebScraping")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebScraping")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
