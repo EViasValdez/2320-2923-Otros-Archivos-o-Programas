369ad2a7e48915a116455e9ab18f95982e1bdcde
Eduardo <eduardoviasvaldez@yahoo.com.mx>
2026-09-18 09:31:23 -0600

    Add files via upload

--- 2823/BatteryChecker_v2/BatteryChecker_v2/obj/Debug/net8.0-windows/BatteryChecker_v2.AssemblyInfo.cs
+++ 2823/BatteryChecker_v2/BatteryChecker_v2/obj/Debug/net8.0-windows/BatteryChecker_v2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BatteryChecker_v2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d74f6240398af9562a113317111bf497fe1a3540")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bf44913fa1c09f0c7fd4fbdf7f4f55d161e7750")]
 [assembly: System.Reflection.AssemblyProductAttribute("BatteryChecker_v2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BatteryChecker_v2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
